0e90d0458d2e8e09059b35fff8c7ebb7514271da
pryugram <87767684+pryugram@users.noreply.github.com>
2021-07-22 15:34:33 +0900

    enhancing1Rm

---
+++ BasicFrame.java
@@ -0,0 +1,63 @@
+
+public class BasicFrame {
+
+	public static void main(String[] args) {
+
+		// Date
+		// workout name
+		
+		System.out.println("date : " + "2021-07-22");
+		System.out.println("workOut : " + "deadLift");
+		System.out.println("currentWeight : " + 65.0); //입력값
+		System.out.println("current8Rm : " + 80.0); //입력값
+		System.out.println("currentReps : " + 10.0); //입력값
+		System.out.println("currentSets : " + 4.0); //입력값
+		System.out.println("current1RM : " + "currentWeight*currentReps*0.024 + current8Rm"); 
+		System.out.println("currentTotalMess : " + (65.0*10.0*4.0 + 80.0*1.0));
+		
+		System.out.println("\ndate : " + "2021-07-29");
+		System.out.println("workOut : " + "deadLift"); 
+		System.out.println("targetWeight : " + 70.0); //입력값
+		System.out.println("current8Rm : " + 80.0); //입력값
+		System.out.println("targetReps : " + 10.0);		//입력값
+		System.out.println("targetSets : " + 5.0);		//입력값
+		System.out.println("target1RM : " + 85.0);		
+		System.out.println("targetTotalMess : " + (65.0*10.0*4.0 + 80.0*1.0));
+		
+		
+		
+//		date = 07.22.2021
+//		workOut = deadLift
+//		currentWeight = 65.0
+//		currentReps = 10.0
+//		currentMaximumWeight = 80.0
+//		currentSets = 4.0
+//		current1Rm = 80.0
+//		currentTotalMess = 65.0*10.0*4.0 + 80.0*1
+		
+//		targetWeight = t1
+//		targetRepetitions = t2
+//		targetSets = t3
+//		target1Rm = 
+//		target total mess = t1*t2*t3 + 
+		
+				
+		
+		// current weight
+		// current repetitions
+		// current set numbers
+		// current 1 repetition maximum
+		// current total mess
+		
+		// target weight
+		// target repetitions
+		// target set numbers
+		// target 1 repetition maximum
+		// target total mess
+		
+		
+		
+		
+	}
+
+}
