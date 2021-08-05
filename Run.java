3f2348dfc56e3c19beb647535391cb79059e6e58
pryugram <87767684+pryugram@users.noreply.github.com>
2021-08-05 23:03:37 +0900

    changed run file

--- Run.java
+++ Run.java
@@ -5,27 +5,27 @@ public class Run {
 	{
 		
 	ControlPractice cp = new ControlPractice();
-	System.out.println("실습문제 1");
+	System.out.println("Practice 1");
 	cp.practice1();
-	System.out.println("실습문제 2");
+	System.out.println("Practice 2");
 	cp.practice2();
-	System.out.println("실습문제 3");
+	System.out.println("Practice 3");
 	cp.pracitce3();
-	System.out.println("실습문제 4");
+	System.out.println("Practice 4");
 	cp.practice4();
-	System.out.println("실습문제 5");
+	System.out.println("Practice 5");
 	cp.practice5();
-	System.out.println("실습문제 6");
+	System.out.println("Practice 6");
 	cp.practice6();
-	System.out.println("실습문제 7");
+	System.out.println("Practice 7");
 	cp.practice7();
-	System.out.println("실습문제 8");
+	System.out.println("Practice 8");
 	cp.practice8();
-	System.out.println("실습문제 9");
+	System.out.println("Practice 9");
 	cp.practice9();
-	System.out.println("실습문제 10");
+	System.out.println("Practice 10");
 	cp.practice10();
-	System.out.println("실습문제 11");
+	System.out.println("Practice 11");
 	cp.practice11();
 	
 	
