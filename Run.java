5dd128759169d35f0a59bca3c6cc8a10b99a38af
pryugram <87767684+pryugram@users.noreply.github.com>
2021-08-09 23:19:59 +0900

    Run file for LoopPractice

--- Run.java
+++ Run.java
@@ -1,34 +1,44 @@
-package com.kh.practice.chap01;
+package com.kh.practice.chap02.run;
+
+import com.kh.practice.chap02.loop.LoopPractice;
 
 public class Run {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
+		LoopPractice lp = new LoopPractice();
+//		System.out.println("실습문제1");
+//		lp.practice1();
+//		System.out.println("실습문제2");
+//		lp.practice2();
+//		System.out.println("실습문제3");
+//		lp.practice3();
+//		System.out.println("실습문제4");
+//		lp.practice4();
+//		System.out.println("실습문제5");
+//		lp.practice5();
+//		System.out.println("실습문제6");
+//		lp.practice6();
+//		System.out.println("실습문제7");
+//		lp.practice7();
+//		System.out.println("실습문제8");
+//		lp.practice8();
+//		System.out.println("실습문제9");
+//		lp.practice9();
+//		System.out.println("실습문제10");
+//		lp.practice10();
+//		System.out.println("실습문제11");
+//		lp.practice11();
+//		System.out.println("실습문제12");
+//		lp.practice12();
+//		System.out.println("실습문제13");
+//		lp.practice13();
+//		System.out.println("실습문제14");
+//		lp.practice14();
+//		System.out.println("실습문제15");
+//		lp.practice15();
+//		System.out.println("실습문제16");
+//		lp.practice16();
 		
-	ControlPractice cp = new ControlPractice();
-	System.out.println("Practice 1");
-	cp.practice1();
-	System.out.println("Practice 2");
-	cp.practice2();
-	System.out.println("Practice 3");
-	cp.pracitce3();
-	System.out.println("Practice 4");
-	cp.practice4();
-	System.out.println("Practice 5");
-	cp.practice5();
-	System.out.println("Practice 6");
-	cp.practice6();
-	System.out.println("Practice 7");
-	cp.practice7();
-	System.out.println("Practice 8");
-	cp.practice8();
-	System.out.println("Practice 9");
-	cp.practice9();
-	System.out.println("Practice 10");
-	cp.practice10();
-	System.out.println("Practice 11");
-	cp.practice11();
-	
-	
-	
+		
 	}
+
 }
